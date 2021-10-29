3042848fe6b981d526dece86d17b93d37e4b8391
Nick <47188168+QVRE@users.noreply.github.com>
2021-10-30 00:17:40 +0300

    Added comments to graphics.c
    Just explained every line in the output function better

--- graphics.c
+++ graphics.c
@@ -20,7 +20,7 @@ F32 *wsin;
 F32 *wcos;
 
 char *grout; //converted sequences get stored here before being printed
-u32 groff; //how filled groff is
+u32 groff; //gr offset. How filled grout is
 
 color blankbuf[512];
 
@@ -31,7 +31,7 @@ static inline void GrInit(u32 grout_size) {
     wcos = &wsin[ROT/4];
 
     grout = malloc(grout_size);
-    grout[0] = '\e', grout[1] = '[', grout[2] = 'H';
+    grout[0] = '\e', grout[1] = '[', grout[2] = 'H'; //first 3 bytes always do home escape code
 }
 
 gr GrBuffer(u32 width,u32 height,u32 xBlank,u32 yBlank) {
@@ -40,9 +40,9 @@ gr GrBuffer(u32 width,u32 height,u32 xBlank,u32 yBlank) {
     buf.pal = malloc((height+yBlank*2)*sizeof(color*)) + yBlank*sizeof(color*);
     buf.pal[0] = calloc((width+xBlank)*height,sizeof(color));
 
-    //make blanking lines and spaces so things like circles don't clip
+    //make blanking areas so that out-of-screen graphics doesn't always crash or clip
     for (int i=1; i<height; i++) {
-        if (i<=yBlank) {
+        if (i<=yBlank) { //assuming yBlank <= height
             buf.pal[-i] = blankbuf;
             buf.pal[height-1+i] = blankbuf;
         }
@@ -55,7 +55,7 @@ gr GrBuffer(u32 width,u32 height,u32 xBlank,u32 yBlank) {
     return buf;
 }
 void GrFill(gr *buf, const color clr) {
-    color *ptr = buf->pal[0];
+    color *ptr = buf->pal[0]; //point to start of graphics buffer
     u32 size = (buf->w+buf->xBlank)*buf->h;
     while (size--)
         *ptr++ = clr;
@@ -86,55 +86,55 @@ void GrTriangle(gr *buf, uvec2 A, uvec2 B, uvec2 C, color clr) {
 }
 
 void GrCircle(gr *b, const uvec2 O, int radius, int skip, color clr) {
-    for (int i=0; i<ROT; i+=skip)
+    for (int i=0; i<ROT; i+=skip) //loop through precomputes
         b->pal[(int)O.y+(int)round(wsin[i]*radius)][(int)O.x+(int)round(wcos[i]*2*radius)] = clr;
 }
 void GrCircleFilled(gr *b, const uvec2 O, const int radius, color clr) {
     for(int y=-radius; y<=radius; y++)
         for(int x=-2*radius; x<=2*radius; x++)
-            if(((x*x)>>2)+y*y <= radius*radius)
+            if(((x*x)>>2)+y*y <= radius*radius) //check if inside
                 b->pal[(int)O.y+y][(int)O.x+x] = clr;
 }
 
-void gremit(u8 val, const u32 end) {
-    u32 pair = end;
-    u32 size = 1;
+void gremit(u8 val, const u32 end) { //convert u8 to ASCII decimal + add an extra char
+    u32 pair = end; //lower part is set to ending character. Things are printed low to high
+    u32 size = 1; //so far, size is 1
     while (val) {
-        pair <<= 8;
-        pair ^= '0' + val % 10;
-        val /= 10;
+        pair <<= 8; //shift to insert new char
+        pair ^= '0' + val % 10; //get our base 10 digit
+        val /= 10; //shift to the right in base 10
         size++;
     }
-    *(u32*)(&grout[groff]) = pair;
+    *(u32*)(&grout[groff]) = pair; //emit pair to grout
     groff += size;
 }
 void draw(gr *buf) {
-    groff = 3;
-    color last = BLACK;
+    groff = 3; //first 3 bytes will always be the escape code to go to 0,0
+    color last = BLACK; //small optimization, kinda similar to RLE
     u32 x=0, y=0;
-    color c;
+    color c; //makes code nicer, accessing this instead of buf->pal[y][x]
 
     draw:
     c = buf->pal[y][x];
-    if (c.r!=last.r || c.g!=last.g || c.b!=last.b) {
-        *(u64*)(&grout[groff]) = 0x3b323b38345b1b; // \e[48;2;
+    if (c.r!=last.r || c.g!=last.g || c.b!=last.b) { //check if this is same as the previous
+        *(u64*)(&grout[groff]) = 0x3b323b38345b1b; // \e[48;2; backwards
         groff += 7;
-        gremit(c.r,';');
-        gremit(c.g,';');
-        gremit(c.b,'m');
+        gremit(c.r,';'); //emit red value in ASCII + a semicolon at the end (for an ESC seq)
+        gremit(c.g,';'); //same with green
+        gremit(c.b,'m'); //complete sequence with m for color
     }
     last = c;
-    grout[groff] = (!c.a * ' ') ^ c.a;
+    grout[groff] = (!c.a * ' ') ^ c.a; //4th byte of color var is used as ASCII char
     groff++;
     x++;
-    if (x==buf->w) {
+    if (x==buf->w) { //at end of line do newline
         grout[groff] = '\n';
         groff++;
         x=0, y++;
-        if (y==buf->h) {
-            *(u32*)(&grout[groff]) = 0x6d305b1b;
+        if (y==buf->h) { //check if this is the last line
+            *(u32*)(&grout[groff]) = 0x6d305b1b; // \e[0m (set color to default)
             groff += 4;
-            write(0,grout,groff);
+            write(0,grout,groff); //finally output our buffer
             return;
         }
     }
